d50e2299b06133b54f1b658b04ed325f01cfc47b
tanishksahni <tanishk.sahni2002@gmail.com>
2024-05-23 11:54:34 +0530

    Initial Commit

---
+++ Starlight/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Starlight
+//
+//  Created by Tanishk Sahni on 23/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Starlight/StarlightApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarlightApp.swift
+//  Starlight
+//
+//  Created by Tanishk Sahni on 23/05/24.
+//
+
+import SwiftUI
+
+@main
+struct StarlightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
