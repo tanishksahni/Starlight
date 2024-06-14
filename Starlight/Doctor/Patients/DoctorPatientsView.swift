f8c2c216a9560d40679c82ccd5cf86603110baf8
Akshat Gulati <103839527+akshat-gulati@users.noreply.github.com>
2024-06-14 14:08:40 +0530

    update

--- Starlight/Doctor/Patients/DoctorPatientsView.swift
+++ Starlight/Doctor/Patients/DoctorPatientsView.swift
@@ -7,47 +7,6 @@
 
 import SwiftUI
 
-//struct DoctorPatientsView: View {
-//    @State private var searchText = ""
-//    
-////    var filteredCards: [Patient] {
-////        if searchText.isEmpty {
-////            return patients
-////        } else {
-////            return patients.filter { $0.name.localizedCaseInsensitiveContains(searchText) }
-////        }
-////    }
-//    
-//    var body: some View {
-//        NavigationStack {
-//            ScrollView {
-////                VStack(spacing: 16) {
-////                    ForEach(patients) { patient in
-////                        NavigationLink(destination: DoctorPatientInfoView()) {
-////                            PatientCardView(data: patient)
-////                        }
-////                        
-////                    }
-////                }
-////                .padding()
-//            }
-//            .scrollIndicators(.hidden)
-//            .navigationTitle("Patients")
-//            .searchable(text: $searchText, prompt: "Search patients")
-//            
-//        }
-//    }
-//}
-//
-//#Preview {
-//    DoctorPatientsView()
-//}
-
-
-
-
-import SwiftUI
-
 struct DoctorPatientsView: View {
     @State private var searchText = ""
     
